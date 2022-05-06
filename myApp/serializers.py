4a3f49507c099c0224a272b3a455ae348dd95f09
amit2301 <amit.kumardebnath@inwebinfo.com>
2022-05-07 02:00:06 +0530

    serializers  added

---
+++ myApp/serializers.py
@@ -0,0 +1,14 @@
+from myApp.models import *
+from rest_framework import serializers
+
+
+class DogSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Dog
+        fields = '__all__'
+
+
+class BreedSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Breed
+        fields = "__all__"
